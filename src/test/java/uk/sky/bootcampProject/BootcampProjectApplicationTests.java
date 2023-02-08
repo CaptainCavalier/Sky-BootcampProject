14da18612784c769477238166bfd6d7a155935c0
CaptainCavalier <anthonyingegnere@googlemail.com>
2023-02-08 16:05:26 +0000

    init commit

---
+++ src/test/java/uk/sky/bootcampProject/BootcampProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.sky.bootcampProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
